e9bb95590155397d38e475a1e2b8a8c5cef14c10
Ryan Sakurai <ryansakurai03@gmail.com>
2024-04-02 19:50:39 -0300

    complete week-1/Desafio do Maior Número

---
+++ week-1/maior_numero.py
@@ -0,0 +1,13 @@
+"""
+Desafio do Maior Número
+
+https://neps.academy/br/exercise/323
+"""
+
+def main():
+    nums = map(int, input().split(" "))
+    print(max(nums))
+
+if __name__=="__main__":
+    main()
+
