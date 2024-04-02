9c1595929a3433f1230b0b91592506182c0d5316
Ryan Sakurai <ryansakurai03@gmail.com>
2024-04-02 20:08:49 -0300

    create week-1/Watermelon first solution

---
+++ week-1/watermelon.py
@@ -0,0 +1,12 @@
+"""
+Watermelon
+
+https://codeforces.com/problemset/problem/4/A
+"""
+
+def main():
+    weight = int(input())
+    print("YES" if weight%2 == 0 else "NO")
+
+if __name__=="__main__":
+    main()
