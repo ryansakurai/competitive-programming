6be4556d0dcc7a008c7f8d2f48d4b9a6ae9324c4
Ryan Sakurai <ryansakurai03@gmail.com>
2024-04-02 19:45:36 -0300

    complete week-1/Flíper

---
+++ week-1/fliper.py
@@ -0,0 +1,11 @@
+def main():
+    door_p, door_r = map(int, input().split())
+    if door_p == 0:
+        print("C")
+    elif door_p == 1 and door_r == 0:
+        print("B")
+    else:
+        print("A")
+
+if __name__=="__main__":
+    main()
