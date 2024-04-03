01ca771df6e560f17dbad5974e9927fea62892b0
Ryan Sakurai <ryansakurai03@gmail.com>
2024-04-02 22:03:32 -0300

    complete week-1/Watermelon

--- week-1/watermelon.py
+++ week-1/watermelon.py
@@ -5,8 +5,15 @@ https://codeforces.com/problemset/problem/4/A
 """
 
 def main():
+    SMALLEST_PIECE = 2
+
     weight = int(input())
-    print("YES" if weight%2 == 0 else "NO")
+    remaining_piece = weight - SMALLEST_PIECE
+
+    if remaining_piece > 0 and remaining_piece%2 == 0:
+        print("YES")
+    else:
+        print("NO")
 
 if __name__=="__main__":
     main()
