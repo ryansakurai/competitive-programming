989ebdd64e676e48080159cc6cd69df0c9d860f9
Ryan Sakurai <ryansakurai03@gmail.com>
2024-04-02 22:06:49 -0300

    refactor week-1/Watermelon

--- week-1/watermelon.py
+++ week-1/watermelon.py
@@ -6,10 +6,9 @@ https://codeforces.com/problemset/problem/4/A
 
 def main():
     SMALLEST_PIECE = 2
+    total_weight = int(input())
 
-    weight = int(input())
-    remaining_piece = weight - SMALLEST_PIECE
-
+    remaining_piece = total_weight - SMALLEST_PIECE
     if remaining_piece > 0 and remaining_piece%2 == 0:
         print("YES")
     else:
