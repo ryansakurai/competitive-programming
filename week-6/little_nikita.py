18d91c7dd2c11cb9baeb86eb84dbfa84e5577333
Ryan Sakurai <ryansakurai03@gmail.com>
2024-07-02 17:09:05 -0300

    complete week 6/Little Nikita

--- week-6/little_nikita.py
+++ week-6/little_nikita.py
@@ -5,8 +5,18 @@ https://codeforces.com/contest/1977/problem/A
 """
 
 def main():
-    # shovel_price, single_coin_value = map(int, input().split(" "))
-    pass
+    qt_cases = int(input())
+    results = []
+
+    for _ in range(qt_cases):
+        qt_moves, final_qt_cubes = map(int, input().split(" "))
+        if qt_moves >= final_qt_cubes and (qt_moves - final_qt_cubes) % 2 == 0:
+            results.append("Yes")
+        else:
+            results.append("No")
+
+    for result in results:
+        print(result)
 
 
 if __name__=="__main__":
