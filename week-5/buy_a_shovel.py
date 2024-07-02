90150efecd4c5c0082ca7726ef4aefd23d7325f1
Ryan Sakurai <ryansakurai03@gmail.com>
2024-07-02 14:35:00 -0300

    complete week 5/Sushi for Two

--- week-5/buy_a_shovel.py
+++ week-5/buy_a_shovel.py
@@ -4,8 +4,11 @@ Buy a Shovel
 https://codeforces.com/contest/732/problem/A
 """
 
+# SHOVEL_PRICE * qt_shovels = 10*qt_coins + SINGLE_COIN_VALUE?
+
 def main():
-    pass
+    shovel_price, single_coin_value = map(int, input().split(" "))
+
 
 
 if __name__=="__main__":
--- week-5/sushi_for_two.py
+++ week-5/sushi_for_two.py
@@ -4,8 +4,41 @@ Sushi for Two
 https://codeforces.com/problemset/problem/1138/A
 """
 
+class SushiType:
+    TUNA = 1
+    EEL = 2
+
+
 def main():
-    pass
+    _ = input()
+    sushis = list(map(int, input().split(" ")))
+
+    largest_sequence = 0
+    
+    i=0
+    while i < len(sushis):
+        seq_first_half = count_sushis(sushis, i)
+        i += seq_first_half
+        seq_second_half = count_sushis(sushis, i)
+        seq_whole = min(seq_first_half, seq_second_half)*2
+
+        largest_sequence = seq_whole if seq_whole > largest_sequence else largest_sequence
+
+    print(largest_sequence)
+
+def count_sushis(sushis: list[SushiType], start_idx: int) -> int:
+    if start_idx >= len(sushis):
+        return 0
+
+    type_to_count = sushis[start_idx]
+
+    qt=0
+    for i in range(start_idx, len(sushis)):
+        if sushis[i] != type_to_count:
+            break
+        qt += 1
+
+    return qt
 
 
 if __name__=="__main__":
