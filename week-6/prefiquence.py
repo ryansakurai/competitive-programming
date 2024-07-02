af39445a08b5d684e497f5e62713ca1b5a5d00aa
Ryan Sakurai <ryansakurai03@gmail.com>
2024-07-02 19:34:15 -0300

    complete week 6/Prefiquence

--- week-6/prefiquence.py
+++ week-6/prefiquence.py
@@ -5,8 +5,25 @@ https://codeforces.com/problemset/problem/1968/B
 """
 
 def main():
-    # shovel_price, single_coin_value = map(int, input().split(" "))
-    pass
+    qt_cases = int(input())
+    results = []
+
+    for _ in range(qt_cases):
+        _ = input()
+        potential_subsequence = input()
+        whole_string = input()
+
+        current_idx = 0
+        for char in whole_string:
+            if current_idx >= len(potential_subsequence):
+                break
+            if char == potential_subsequence[current_idx]:
+                current_idx += 1
+
+        results.append(current_idx)
+
+    for result in results:
+        print(result)
 
 
 if __name__=="__main__":
