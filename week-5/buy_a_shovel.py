a0f11cd33040b128c73109f7971c7107e59b556d
Ryan Sakurai <ryansakurai03@gmail.com>
2024-07-02 14:57:03 -0300

    complete week 5/Buy a Shovel

--- week-5/buy_a_shovel.py
+++ week-5/buy_a_shovel.py
@@ -9,6 +9,15 @@ https://codeforces.com/contest/732/problem/A
 def main():
     shovel_price, single_coin_value = map(int, input().split(" "))
 
+    qt_shovels = 1
+    while True:
+        if qt_shovels*shovel_price % 10 == 0:
+            break
+        if (qt_shovels*shovel_price - single_coin_value) % 10 == 0:
+            break
+        qt_shovels += 1
+
+    print(qt_shovels)
 
 
 if __name__=="__main__":
