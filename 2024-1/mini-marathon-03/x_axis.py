e940a2c57bff0ce5b0f9c7093f3e5cf811d1072f
Ryan Sakurai <ryansakurai03@gmail.com>
2024-10-16 14:13:51 -0300

    complete mini marathon III/X Axis

--- 2024-1/mini-marathon-03/x_axis.py
+++ 2024-1/mini-marathon-03/x_axis.py
@@ -5,7 +5,19 @@ https://codeforces.com/contest/1986/problem/A
 """
 
 def main():
-    pass
+    qt_cases = int(input())
+    results = []
+
+    for _ in range(qt_cases):
+        x = list(map(int, input().split(" ")))
+
+        x.sort()
+        total_dist = abs(x[0] - x[1]) + abs(x[1] - x[2])
+
+        results.append(total_dist)
+
+    for result in results:
+        print(result)
 
 
 if __name__=="__main__":
