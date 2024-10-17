30b84076eb36e9e2deac76d31593f862f4a57c63
Ryan Sakurai <ryansakurai03@gmail.com>
2024-10-17 14:02:02 -0300

    complete mini marathon I/Decoding

--- 2024-1/mini-marathon-01/decoding.py
+++ 2024-1/mini-marathon-01/decoding.py
@@ -5,8 +5,16 @@ https://codeforces.com/contest/746/problem/B
 """
 
 def main():
-    pass
+    _ = input()
 
+    coded_word = input()
 
-if __name__=="__main__":
+    decoded_letters = []
+    for letter in coded_word[::-1]:
+        decoded_letters.insert(len(decoded_letters) // 2, letter)
+
+    decoded_word = "".join(decoded_letters)
+    print(decoded_word)
+
+if __name__ == "__main__":
     main()
