391a040d0f9480934a72e35dd33100678b0a6d1a
Ryan Sakurai <ryansakurai03@gmail.com>
2024-10-18 13:34:54 -0300

    complete mini marathon II/Problem Gerator

--- 2024-1/mini-marathon-02/e01_problem_generator.py
+++ 2024-1/mini-marathon-02/e01_problem_generator.py
@@ -5,7 +5,23 @@ https://codeforces.com/contest/1980/problem/A
 """
 
 def main():
-    pass
+    qt_cases = int(input())
+    results = []
+
+    for _ in range(qt_cases):
+        _, qt_rounds = map(int, input().split())
+        problems = list(input())
+
+        qt_problems = 0
+        for letter in ("A", "B", "C", "D", "E", "F", "G"):
+            new_problems = qt_rounds - problems.count(letter)
+            if new_problems > 0:
+                qt_problems += new_problems
+
+        results.append(qt_problems)
+
+    for result in results:
+        print(result)
 
 
 if __name__=="__main__":
