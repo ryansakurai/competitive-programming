05e9840e92b5dada9ee06179b856ccab895fb4d6
Ryan Sakurai <ryansakurai03@gmail.com>
2024-10-18 14:04:52 -0300

    complete mini marathon II/Presents

--- 2024-1/mini-marathon-02/e02_presents.py
+++ 2024-1/mini-marathon-02/e02_presents.py
@@ -5,7 +5,16 @@ https://codeforces.com/contest/136/problem/A
 """
 
 def main():
-    pass
+    _ = input()
+    receivers_from_input = tuple(map(int, input().split()))
+    receivers = tuple(receiver - 1 for receiver in receivers_from_input)
+    gifters = [None] * len(receivers)
+
+    for i, receiver in enumerate(receivers):
+        gifters[receiver] = i
+
+    gifters_for_output = tuple(str(gifter + 1) for gifter in gifters)
+    print(" ".join(gifters_for_output))
 
 
 if __name__=="__main__":
