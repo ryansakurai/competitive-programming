e8e6d1ad0788ef32ac2672738a84730117467f15
Ryan Sakurai <ryansakurai03@gmail.com>
2024-10-17 21:21:16 -0300

    complete mini marathon I/Twins

--- 2024-1/mini-marathon-01/e02_twins.py
+++ 2024-1/mini-marathon-01/e02_twins.py
@@ -5,7 +5,18 @@ https://codeforces.com/problemset/problem/160/A
 """
 
 def main():
-    pass
+    _ = input()
+    coins = list(map(int, input().split()))
+
+    his_coins = coins
+    my_coins = []
+
+    his_coins.sort()
+    while sum(his_coins) >= sum(my_coins):
+        coin_taken = his_coins.pop()
+        my_coins.append(coin_taken)
+
+    print(len(my_coins))
 
 
 if __name__=="__main__":
