5b91e06d1ccf9abb1726fa1d2c5e5a8fee5b3e44
Ryan Sakurai <ryansakurai03@gmail.com>
2024-10-17 22:15:31 -0300

    complete mini marathon I/Football

--- 2024-1/mini-marathon-01/e03_football.py
+++ 2024-1/mini-marathon-01/e03_football.py
@@ -5,7 +5,24 @@ https://codeforces.com/problemset/problem/96/A
 """
 
 def main():
-    pass
+    players = input()
+
+    dangerous = False
+    sequence = 1
+    prev_player = None
+    for i, _ in enumerate(players):
+        if prev_player:
+            if players[i] == players[i-1]:
+                sequence += 1
+            else:
+                sequence = 1
+        prev_player = players[i]
+
+        if sequence >= 7:
+            dangerous = True
+            break
+
+    print("YES" if dangerous else "NO")
 
 
 if __name__=="__main__":
